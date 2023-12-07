759d34f7fb37b55e005a7e3acbedbcae8f9c2060
Bob Paauwe <bpaauwe@yahoo.com>
2023-12-07 09:54:59 -0800

    Additional methods to discover speakers.
    In addition to the automated discovery of speakers add the
    ability to parse custom parameters with speaker name/ip address.
    
    Also add the ability for the automated discovery to scan specific
    subnets as the original discovery would only scan the submet that
    the Polisy/eisy is on.
    
    This also fixes a bug in the poll function so that the status
    of the speakers is queried and updated at the short poll interval.

--- README.md
+++ README.md
@@ -1,7 +1,47 @@
 # UDI-Sonos-Poly
 
-This is a Polyglot v3 NodeServer for the ISY to control your Sonos speakers.
+This is a Polyglot v2 NodeServer for the ISY 994i version 5+ to control your Sonos speakers.
 
-Install through the NodeServer Store in Polyglot v3.
+Install through the NodeServer Store in Polyglot v2.
 
 Please report any issues on the [UDI user forum](https://forum.universal-devices.com/topic/18643-polyglot-sonos-nodeserver/).
+
+
+If the default discovery of the Sonos speakers does not work, you can use 2 alternate methods seperately or together.  These are helpful when you are running a VLAN and can't see the discovery messages from the Sonos speakers but can control them by IP address.
+
+
+Manual Speaker Entries Method
+
+If the discover does not work, or you prefer to not use it, you can add customParms in the Polyglot Web UI to tell it about your speakers.
+
+Create a param(s) with:
+
+name = sonos_"uniqueid" where uniqueid is the address that will be used for the ISY node, example.  sonos_LivingSonos
+value like: { "name": "Sonos Living Room", "host": "192.168.1.86" }
+
+Anytime these params are added or modified you need to run the 'Discover' on the Controller node.
+
+
+Extended network scanning method
+
+You an instruct the server to scan other networks than the network your EISY or Polisy are running Polyglot 3
+
+Create a param with:
+
+
+name = network_to_scan
+value = a subnet in CIDR Subnet mask notation.  
+
+example:
+name = network_to_scan
+value = 192.168.20.1/24   - No quotes around the text, no brackets, just the text 
+
+If you want to scan multiple subnets you can use list multiple
+
+example:
+name = network_to_scan
+value = 192.168.20.1/24,192.168.30.1/24    - No quotes around the text, no brackets, just the text
+
+So if your EISY is running on 192.168.1.1, then it will search all IP addresses on 192.168.1.1 by default and then 
+search on subnets listed.
+
--- sonos-poly.py
+++ sonos-poly.py
@@ -9,8 +9,12 @@ import sys
 import soco
 import requests
 import json
+#add re library
+import re
 
 LOGGER = udi_interface.LOGGER
+# DD need to get custom params
+Custom = udi_interface.Custom
 
 with open('server.json') as data:
     SERVERDATA = json.load(data)
@@ -22,20 +26,50 @@ except (KeyError, ValueError):
     VERSION = '0.0.0'
 
 class Controller(object):
+	# DD - only need id
+	#id = 'sonosctl'
     def __init__(self, polyglot):
         self.name = 'Sonos Controller'
+        # DD net to get custom params / add controller node
+        self.poly = polyglot
         self.discovery = False
-
+        self.Params = Custom(polyglot, 'customparams')
+        polyglot.subscribe(polyglot.CUSTOMPARAMS, self.parameterHandler)
+        polyglot.ready()
+        self.start()
+        ## DD not sure if we need to add the controller node
+		#self.poly.addNode(self)
+				
     def start(self):
         LOGGER.info('Starting Sonos Polyglot v3 NodeServer version {}, udi_interface: {}'.format(VERSION, udi_interface.__version__))
         self.discover()
 
+    # DD add routine to load parameters
+    def parameterHandler(self, params):
+        LOGGER.info("running parameterHandler")
+        self.Params.load(params)
+        
+	## strip special characters from names	
+    def get_valid_node_name(self, name):
+        name = bytes(name, 'utf-8').decode('utf-8','ignore')
+        return re.sub(r"[<>'~!@#$%^&*(){}[\]?/\\;:\"']+","",name)
 
     def discover(self, command = None):
         LOGGER.info('Starting Speaker Discovery...')
         polyglot.Notices.clear()
         self.discovery = True
-        speakers = soco.discover()
+        netscan = False 
+        kwargs = {}
+        for netparam in self.Params:
+            LOGGER.info('Starting network scan')
+            match = re.match( "networks_to_scan", netparam, re.I)
+            if match is not None:
+                netscanlist = self.Params[netparam].split(",")
+                kwargs = {"networks_to_scan": netscanlist}
+                netscan = True
+
+        speakers = soco.discover(5,False,None,"Sonos",netscan,**kwargs)
+
         if speakers:
             LOGGER.info('Found {} Speaker(s)'.format(len(speakers)))
             for speaker in speakers:
@@ -47,6 +81,43 @@ class Controller(object):
         else:
             LOGGER.info('No Speakers found. Are they powered on?')
             polyglot.Notices['error'] = 'No speakers found. Make sure they are powered on and try Disover again.'
+
+        # DD - Add manual nodes
+        LOGGER.info('add speakers from custom parameters')
+        #
+        for param in self.Params:
+            # Look for customParam starting with sonos_
+            match = re.match( "sonos_(.*)", param, re.I)
+            LOGGER.info('param={} match={}'.format(param,match))
+            if match is not None:
+                # The Sonos address is everything following the sonos_
+                address = match.group(1).lower()
+                LOGGER.info('process param={0} address={1}'.format(param,address))
+                # Get the customParam value which is json code
+                #  { "name": "Sonos FamilyRoom", "host": "192.168.1.86" }
+                cfg = self.Params[param]
+                cfgd = None
+                try:
+                    cfgd = json.loads(cfg)
+                except:
+                    err = sys.exc_info()[0]
+                    LOGGER.error('failed to parse cfg={0} Error: {1}'.format(cfg,err))
+                if cfgd is not None:
+                    # Check that name and host are defined.
+                    addit = True
+                    if not 'name' in cfgd:
+                        LOGGER.error('No name in customParam {0} value={1}'.format(param,cfg))
+                        addit = False
+                    if not 'host' in cfgd:
+                        LOGGER.error('No host in customParam {0} value={1}'.format(param,cfg))
+                        addit = False
+                    if addit:
+                        sonos_name = self.get_valid_node_name(cfgd['name'])
+                        sonos_ip = self.get_valid_node_name(cfgd['host'])
+                        if not polyglot.getNode(address): 
+                            polyglot.addNode(Speaker(polyglot, address, address, sonos_name, sonos_ip))
+                        else:
+                            LOGGER.info('Speaker {} already configured.'.format(sonos_name))
         self.discovery = False
 
 
@@ -63,21 +134,25 @@ class Speaker(udi_interface.Node):
 
     def start(self):
         LOGGER.info("{} ready to rock!".format(self.name))
-        self.update('shortpoll')
+        self.update('shortPoll')
 
     def update(self, pollflag):
-        if pollflag == 'shortpoll':
+        if pollflag == 'shortPoll':
             try:
                 self.setDriver('ST', self._get_state())
                 self.setDriver('SVOL', self.zone.volume)
                 self.setDriver('GV1', self.zone.bass)
                 self.setDriver('GV2', self.zone.treble)
+                muteval = 1 if self.zone.mute == True else 0 
+                self.setDriver('GV3', muteval)
+                #LOGGER.info(self.zone.player_name)
+                #LOGGER.info('Mute Status %s', self.zone.mute)
             except requests.exceptions.ConnectionError as e:
                 LOGGER.error('Connection error to Speaker or ISY.: %s', e)
 
 
     def query(self, command=None):
-        self.update('shortpoll')
+        self.update('shortPoll')
         self.reportDrivers()
 
     def _get_state(self):
@@ -130,9 +205,11 @@ class Speaker(udi_interface.Node):
 
     def _mute(self, command):
         self.zone.mute = True
+        self.setDriver('GV3', 1)
 
     def _unmute(self, command):
         self.zone.mute = False
+        self.setDriver('GV3', 0)
 
     def _volume(self, command):
         try:
@@ -166,7 +243,8 @@ class Speaker(udi_interface.Node):
     drivers = [{'driver': 'GV1', 'value': 0, 'uom': '56'},
                 {'driver': 'GV2', 'value': 0, 'uom': '56'},
                 {'driver': 'SVOL', 'value': 0, 'uom': '51'},
-                {'driver': 'ST', 'value': 0, 'uom': '25'}]
+                {'driver': 'ST', 'value': 0, 'uom': '25'},
+                {'driver': 'GV3', 'value': 0, 'uom': '2'}]
 
     commands = {    'PLAY': _play,
                     'STOP': _stop,
